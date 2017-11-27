57569acfaede47f2ba5400e94072e75420166e72
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-27 14:56:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,16 +2,17 @@
 include Math
 
 def prime?(num)
-  i = 2
-  while i <= Math.sqrt(num).floor
-    if num <= 1
-    	return false
-    elsif ((i**num)%num == i%num)
-    	return true
-    else
-      return false
-    end
+  if num <= 1
+    return false
+  else
+    i = 2
+    while i <= Math.sqrt(num).floor
+      if ((i**num)%num == i%num)
+      	return true
+      else
+        return false
+      end
 
-    i+=1
-  end
+      i+=1
+    end
 end
