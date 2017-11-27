a9b70e9eec44ad9b3d4900c26ed6c8630bc5b54e
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-27 14:46:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,18 @@
 require 'math'
 
 def prime
+  if num <= 1
+  	return false
+  end
 
+  i = 2
+  while i <= Math.sqrt(num).floor
+    if (i**num % num == i%num)
+    	return true
+    else
+      return false
+    end
+
+    i+=1
+  end
 end
